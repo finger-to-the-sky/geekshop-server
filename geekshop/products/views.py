440b43ec322d7a90b9d92ee2a05f39061f8f774a
zagmak <mzagrebam@gmail.com>
2022-08-04 20:51:46 +0300

    completed the second lesson

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+import json
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+
+    with open('/home/zagmak/PycharmProjects/geekshop-server/geekshop/products/fixtures/products.json', 'r',
+              encoding='UTF-8') as f:
+        data = json.load(f)
+
+    context = {
+        'title': 'GeekShop - Продукты',
+        'products': []
+    }
+    for item in data:
+        context['products'].append(item['fields'])
+
+    return render(request, 'products/products.html', context)
