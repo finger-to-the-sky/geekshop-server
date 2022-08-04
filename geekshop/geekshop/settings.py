6790914bfe14c19dbcf9b97c6c935660d3366e6e
zagmak <mzagrebam@gmail.com>
2022-08-04 14:08:03 +0300

    append styles

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_project',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
