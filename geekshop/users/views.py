a8c9faf6ee84af8abdf194f8961dc088e233b9a1
zagmak <mzagrebam@gmail.com>
2022-08-07 14:59:13 +0300

    added logic for login process

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from .forms import UserLoginFrom
-def login(request):
-    form = UserLoginFrom()
 
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginFrom(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+
+        form = UserLoginFrom()
     context = {
-        'title': 'GeekShop - Авторизация',
-        'form' : form,
-    }
+        'title': 'GeekShop - Авторизация', 'form': form}
 
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
     context = {
-        'title': 'GeekShop - Регистрация'
-    }
+        'title': 'GeekShop - Регистрация', }
 
     return render(request, 'users/register.html', context)
