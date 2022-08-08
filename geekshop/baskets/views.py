36f31a39fa2ff161899546dd051b12581e1c0b44
zagmak <mzagrebam@gmail.com>
2022-08-08 16:00:04 +0300

    add total_sum and total_quantity

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -12,9 +12,14 @@ def basket_add(request, product_id):
     if not baskets:
         Basket.objects.create(user=request.user, product=product, quantity=1)
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
     else:
+
         basket = baskets.first()
-        basket.quantity += 1
+        if basket.quantity <= product.quantity:
+            basket.quantity += 1
+        else:
+            basket.quantity == product.quantity
         basket.save()
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
