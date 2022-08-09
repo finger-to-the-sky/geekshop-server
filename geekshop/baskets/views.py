1c3eb5887cc1de509cd20e43fa55cc4e29d684f9
zagmak <mzagrebam@gmail.com>
2022-08-09 13:37:52 +0300

    add ajax in basket_quantity

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 from products.models import Product
 from .models import Basket
@@ -27,3 +29,22 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+@login_required
+def basket_edit(request, id, quantity):
+    if is_ajax(request=request):
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+        baskets = Basket.objects.filter(user=request.user)
+        context = {
+            'baskets': baskets
+        }
+        result = render_to_string('basket/basket.html', context)
+        return JsonResponse({'result': result})
