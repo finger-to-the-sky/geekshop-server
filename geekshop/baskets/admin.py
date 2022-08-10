528401130848b7158e97cd04f5991b7284d8eae2
zagmak <mzagrebam@gmail.com>
2022-08-11 00:34:31 +0300

    add BasketAdminInline

--- geekshop/baskets/admin.py
+++ geekshop/baskets/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Basket
 
-admin.site.register(Basket)
+class BasketAdminInLine(admin.TabularInline):
+    model = Basket
+    fields = readonly_fields = ('product', 'quantity', 'created_timestamp')
+    extra = 0
--- geekshop/users/admin.py
+++ geekshop/users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from .models import User
+from baskets.admin import BasketAdminInLine
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (BasketAdminInLine, )
