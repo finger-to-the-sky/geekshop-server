fe8bb0783c1e0da927b4d93c2c4607ac1f37e62f
zagmak <mzagrebam@gmail.com>
2022-08-10 20:39:38 +0300

    add controller admins_users

--- geekshop/admins/urls.py
+++ geekshop/admins/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from admins.views import index
+from admins.views import index, admins_users
 
 app_name = 'admins'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('users/', admins_users, name='admins_users'),
 ]
--- geekshop/admins/views.py
+++ geekshop/admins/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from users.models import User
+
+
 def index(request):
     context = {
         'title': 'GeekShop - Admin'
     }
     return render(request, 'admins/index.html', context)
+
+
+
+# Read Controller
+def admins_users(request):
+    users = User.objects.all()
+
+    context = {
+        'title': 'GeekShop - Admin',
+        'users': users,
+    }
+    return render(request, 'admins/admin-users-read.html', context)
