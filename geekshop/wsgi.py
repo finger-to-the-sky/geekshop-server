f13bca8fdbee2c4d5eec123bb81083ca11949357
mzagr <mzagrbam@gmail.com>
2023-07-12 00:31:25 +0300

    refactoring

--- geekshop/wsgi.py
+++ geekshop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for geekshop project.
+WSGI config for geekshop2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geekshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geekshop2.settings')
 
 application = get_wsgi_application()
